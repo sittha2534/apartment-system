415fd044219445dcffe915ad4200b896c88068f4
Sittha Juntakun <r_copykakashi@hotmail.com>
2025-09-20 09:10:40 +0700

    Update project including rooms.db and setup scripts

--- app.py
+++ app.py
@@ -286,8 +286,8 @@ def test_db():
     except Exception as e:
         return f"❌ Cannot connect to DB: {e}"
 
-# ------------------------------
-# รัน Flask
-# ------------------------------
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
